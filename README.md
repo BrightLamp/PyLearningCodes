fac23cdf3e1586a5d88a7a338802b35accf4bbf0
Your Name <you@example.com>
2020-05-03 22:17:24 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PyLearningCodes
+
+$$
+a_p
+$$
---
+++ ros_tf_liscener.py
@@ -0,0 +1,18 @@
+# encoding=utf-8
+
+import rospy
+import math
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('py_tf_turtle')
+    listener = tf.TransformListener()
+
+    listener.waitForTransform("/base_link", "/front_caster", rospy.Time(), rospy.Duration(4))
+
+    (trans, rot) = listener.lookupTransform('/base_link', '/front_caster', rospy.Time(0))
+
+    rospy.loginfo('距离原点的位置: x=%f ,y= %f，z=%f', trans[0], trans[1], trans[2])
+    rospy.loginfo('旋转四元数: w=%f ,x= %f，y=%f z=%f', rot[0], rot[1], rot[2], rot[3])
+    canTransform = listener.canTransform('/base_link', '/front_caster', rospy.Time(0))
+    print canTransform
